892861e8028fd0e40b28ae3639f960e5ca2f68b7
lirendada <2916776007@qq.com>
2025-08-17 10:31:56 +0800

    完成了计算属性的案例：全选反选

---
+++ 25_7_21_vue-engineering-way/src/3_2_计算属性的完整写法.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <input type="text" v-model="uname1"></input>
+    <br></br>
+    <input type="text" v-model="uname2"></input>
+</div>
+</template>
+
+<script setup>
+    import { computed } from 'vue';
+
+    // 简易写法，默认为只读，不能修改视图
+    const uname1 = computed(() => {
+        return "liren";
+    })
+
+    // 完整写法，写出两个对象：get 和 set
+    const uname2 = computed({
+        get() {
+            return "liren"
+        }, 
+        set(val) {
+            console.log(val)
+        }
+    })
+</script>
+
+<style scoped>
+
+</style>
