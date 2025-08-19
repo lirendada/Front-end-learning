d3c09d02ee5fc660f042c4fe0a77ef8846fb3c4b
lirendada <2916776007@qq.com>
2025-08-19 20:27:06 +0800

    完成了组件通信中的父传子通信，使用自定义属性以及defineProps

---
+++ 25_7_21_vue-engineering-way/src/05-scoped+组件通信+props校验+记事本组件版/components1/MyGoods.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="item">
+      <img
+        :src="urlImg"
+        :alt="title" />
+      <p class="name">{{ title }}</p>
+      <p class="price">{{ price }}.00</p>
+    </div>
+</template>
+
+<script setup>
+    defineProps([
+        'urlImg', 'title', 'price'
+    ])
+</script>
+
+<style scoped>
+.item {
+  width: 240px;
+  margin-left: 10px;
+  padding: 20px 30px;
+  transition: all 0.5s;
+  margin-bottom: 20px;
+  .item:nth-child(4n) {
+    margin-left: 0;
+  }
+  &:hover {
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+    transform: translate3d(0, -4px, 0);
+    cursor: pointer;
+  }
+  img {
+    width: 100%;
+  }
+  .name {
+    font-size: 18px;
+    margin-bottom: 10px;
+    color: #666;
+  }
+  .price {
+    font-size: 22px;
+    color: firebrick;
+  }
+  .price::before {
+    content: '¥';
+    font-size: 22px;
+  }
+}
+</style>
