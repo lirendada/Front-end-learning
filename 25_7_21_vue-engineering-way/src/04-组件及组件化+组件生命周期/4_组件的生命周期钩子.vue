a87a58f53d81ad6a2bdf9921f760c82456ed0db0
lirendada <2916776007@qq.com>
2025-08-19 11:35:54 +0800

    学习了vue2中组件的生命周期钩子，是选项式的API

---
+++ 25_7_21_vue-engineering-way/src/04-组件及组件化+组件生命周期/4_组件的生命周期钩子.vue
@@ -0,0 +1,13 @@
+<template>
+    <life-cycle v-if="isAlive"/>
+</template>
+
+<script setup>
+    import {ref} from 'vue'
+    import LifeCycle from './components4/LifeCycle.vue';
+    const isAlive = ref(true);
+</script>
+
+<style scoped>
+
+</style>
---
+++ 25_7_21_vue-engineering-way/src/04-组件及组件化+组件生命周期/components4/LifeCycle.vue
@@ -0,0 +1,75 @@
+<template>
+<div>
+    <p>{{ count }}</p>
+    <button @click="count++">+1</button>
+</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return { count:0 }
+        },
+        methods: {
+            fn() { 
+                console.log('执行methods中的fn()') 
+            }
+        },
+
+        setup() {
+            console.log('setup');
+        },
+
+        beforeCreate() {
+            console.log('beforeCreate');
+            console.log(this.count);
+            console.log(this.fn);
+            
+        },
+        created() {
+            console.log('created');
+            console.log(this.count);
+            console.log(this.fn);
+            this.fn();
+
+            this.timerId = setInterval(() => {
+                console.log(this.count);
+            }, 1000)
+        },
+
+        beforeMount() {
+            console.log('beforeMount');
+            console.log(document.querySelector('p'));
+        },
+        mounted() {
+            console.log('mounted');
+            console.log(document.querySelector('p'));
+            document.querySelector('p').style.color = 'red'
+            console.log(document.querySelector('p').innerHTML);
+        },
+
+        beforeUpdate() {
+            console.log('beforeUpdate');
+            console.log(this.count);
+            console.log(document.querySelector('p').innerText);
+        },
+        updated() {
+            console.log('updated');
+            console.log(this.count);
+            console.log(document.querySelector('p').innerText);
+        },
+
+        beforeUnmount() {
+            console.log('beforeUnmount');
+            clearInterval(this.timerId);
+        },
+        unmounted() {
+            console.log('unmounted');
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
