a82e4a10e1d981e0d480f6ced232a370018bbf51
lirendada <2916776007@qq.com>
2025-08-30 17:36:47 +0800

    学习了vue-router中的更多配置，包括路由模式、404页面配置、以及重定向

--- 25_7_21_vue-engineering-way/src/router1/index.js
+++ 25_7_21_vue-engineering-way/src/router1/index.js
@@ -1,17 +1,23 @@
 // 导入两个相关函数
 // createRouter(): 创建路由实例
 // createWebHashHistory(): 创建哈希模式的路由, 路径带 #
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 // 导入 3 个页面组件
 import Find from '@/views1/Find.vue'
 import Friend from '@/views1/Friend.vue'
 import My from '@/views1/My.vue'
+import _404 from '@/views1/404.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(),
+//   history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     // 路由表规则, 即 path 与 component 的对应关系
+    {
+        path: '/',
+        redirect: '/find'
+    },
     {
       path: '/find',
       component: Find
@@ -24,6 +30,10 @@ const router = createRouter({
         name: 'Friend',
         path: '/friend/:fid',
         component: Friend
+    },
+    {
+        path: '/:pathmatch(.*)*',
+        component: _404
     }
   ]
 })
---
+++ 25_7_21_vue-engineering-way/src/views1/404.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="not-found">
+    <h3>404</h3>
+    <p> 你访问的页面去了月球 </p>
+    <router-link to="/"> 去首页 </router-link>
+</div>
+</template>
+
+<script setup>
+        
+</script>
+
+<style scoped>
+
+</style>
